55e70ff53a107097574acc806850fd58c06f9546
Julie Chua <30973531+julsyjuls@users.noreply.github.com>
2025-10-16 14:42:51 +0800

    Delete functions/api directory

--- functions/api/hello.ts
+++
@@ -1,5 +0,0 @@
-export const onRequestGet = ({ request }: { request: Request }) =>
-  new Response(
-    JSON.stringify({ ok: true, path: new URL(request.url).pathname }),
-    { headers: { "Content-Type": "application/json" } }
-  );
